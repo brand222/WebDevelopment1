758eacab2905375650def02594167140264abee0
Brandon <Brandon_McDonald22@yahoo.com>
2020-02-09 12:38:02 -0600

    Added Javascript folder structure

---
+++ JavaScript/main.js
@@ -0,0 +1,32 @@
+//this is a message to the console
+console.log('hello world');
+//this is an error to the console
+console.error('this is an error');
+//this is a warning to the console
+console.warn('This is a warning')
+
+// var = globally scoped
+
+//start using 'let' going forward
+
+//always use constants unless you know you are going to reassign the variable
+const score = 10;
+console.log(score);
+
+//data types = Strings, Numbers, Booleans, null, undefined
+
+const name = 'Brandon';
+const age = 29;
+ console.log('my name is ' + name);
+ const isCool = true;
+ const x = null;
+ const y = undefined;
+ let z;
+
+
+ //typeof returns the datatype of the variable (value) you pass
+ console.log(typeof name);
+ console.log(typeof x);
+ console.log(age);
+ console.log(typeof y);
+ console.log(typeof z);
